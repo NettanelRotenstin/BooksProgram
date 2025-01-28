20d3607231a3a9628f26327cd59656b4f8bfbaba
Netanel Rotenstein <email@example.com>
2025-01-28 12:15:21 +0200

    adding base for express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log(1)
+//import exress
+const express = require("express");
+
+const app = express()
+
+app.use(express.json())
+
+app.use('/',()=>{})
+
+app.listen(1212, () => {
+    console.log(`server is running on port 1212`)
+})
