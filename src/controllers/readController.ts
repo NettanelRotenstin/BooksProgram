7a9d5d543d671439dbb178b370509009025cc95a
Netanel Rotenstein <email@example.com>
2025-01-28 16:08:55 +0200

    all books controller

--- src/controllers/readController.ts
+++ src/controllers/readController.ts
@@ -1,14 +1,25 @@
 import { Request, Response } from "express"
-import { getBookService } from "../services/getService"
+import { getAllBooksService, getBookService } from "../services/getService"
 
 const { Request, Response } = require("express")
 export const getBook = async (req: Request, res: Response): Promise<void> => {
     try {
         const resFromGetService = await getBookService(req.params.id)
         res.status(200).json({
-           resFromGetService
-    })
+            resFromGetService
+        })
     } catch (error) {
         console.log(error)
+    }
+}
+
+export const getAllBooks = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const allBooks = await getAllBooksService()
+        res.status(200).json({
+            allBooks
+        })
+    } catch (error) {
+        
     }
 }
