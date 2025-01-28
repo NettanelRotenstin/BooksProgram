3cdca8f7edf06d8003b39b160f81163d66c7f23c
Netanel Rotenstein <email@example.com>
2025-01-28 16:02:08 +0200

    route for get

--- src/routes/crdRouter.ts
+++ src/routes/crdRouter.ts
@@ -1,5 +1,6 @@
 import { deleteBook } from "../controllers/deleteController"
 import { addBook } from "../controllers/postController"
+import { getBook } from "../controllers/readController"
 
 const {Router} = require('express')
 
@@ -11,6 +12,6 @@ router.post('/POST/books',addBook)
 //delete book route
 router.delete('/DELETE/books/:id',  deleteBook)
 
-
-
+//get book route
+router.get('/GET/books/:id',getBook)
 export default router
