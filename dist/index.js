b99d22c9708e9bc20391f453f6b2b34325922539
Netanel Rotenstein <email@example.com>
2025-01-28 12:15:42 +0200

    adding base of express

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,9 @@
 "use strict";
-console.log(1);
+//import exress
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.use('/', () => { });
+app.listen(1212, () => {
+    console.log(`server is running on port 1212`);
+});
