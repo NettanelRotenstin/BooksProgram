a4251a30044a01cff3ca6692f9ee4bd7128373da
Netanel Rotenstein <email@example.com>
2025-02-02 14:08:53 +0200

    BL post book for organize object

--- src/services/postService.ts
+++ src/services/postService.ts
@@ -5,13 +5,13 @@ import { Book } from "../types/Book";
 //add book service include validation
 export const addBookService = async (book: Book) => {
     try {
-        if(typeof (book.name) === typeof ("") && book.name != "") {
+        if (typeof (book.name) === typeof ("") && book.name != "" && typeof (book.author) == typeof ("") && book.author != "" && typeof (book.publishYear) == typeof (1) && typeof (book.price) == typeof (1)) {
             book.id = v4()
             booksArrey.push(book)
             console.log(booksArrey)
             return book
         }
-        else{
+        else {
             return "Book not addad"
         }
     } catch (error) {
