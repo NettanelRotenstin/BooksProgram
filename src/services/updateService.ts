c3fea1ddf40c8f9dcbf1d73a567464adbccbcb9c
Netanel Rotenstein <email@example.com>
2025-02-02 14:15:18 +0200

    valisation of update book

--- src/services/updateService.ts
+++ src/services/updateService.ts
@@ -5,14 +5,18 @@ export const updateBookService = async (bookId: string, book: Book) => {
     try {
         let bookToUpdate: Book | undefined = await booksArrey.filter(b => b.id == bookId)[0]
         if (!bookToUpdate) {
-            throw new Error('book not found!')           
+            throw new Error('book not found!')
         }
-        bookToUpdate = {...bookToUpdate,...book}
+        bookToUpdate = { ...bookToUpdate, ...book }
+        if ((bookToUpdate.name || bookToUpdate.author == "") || (!bookToUpdate.publishYear || bookToUpdate.publishYear > 2025) || (!bookToUpdate.price || bookToUpdate.price < 0)) {
+            throw new Error('Invalid book data!')
+        }
         let newArr = await booksArrey.filter(b => b.id != bookToUpdate.id)
         newArr.push(bookToUpdate)
         updateArr(newArr)
         return bookToUpdate
     } catch (error) {
+        throw error
 
     }
 
